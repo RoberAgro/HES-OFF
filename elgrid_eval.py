3017c46459a2f4da57675eb701ad995bbffc6964
Erick F. Alves <44233260+efantnu@users.noreply.github.com>
2021-02-04 18:29:26 +0100

    Backup 2021-02-04, review  classes + elgridEval

--- elgrid_eval.py
+++ elgrid_eval.py
@@ -9,125 +9,52 @@ class ES:
 
     Parameters
     ----------
-    powerCharge : float
+    outMax : float
         Rated power for charging in Watts.
-    powerDisch : float
+    outMin : float
         Rated power for discharging in Watts.        
-    rocPCharge : float
-        Maximum rate of change of power for charging in Watts per second.
-    rocPDisch : float
-        Maximum rate of change of power for discharging in Watts per second.        
-            
-    '''
-    def __init__(self, powerCharge, powerDisch, rocPCharge, rocPDisch):
-        self.__initComplete = False
-        self.powerCharge = powerCharge
-        self.powerDisch = powerDisch
-        self.rocPCharge = rocPCharge
-        self.rocPDisch = rocPDisch
-
-        self.__initComplete = True
-
-    @property
-    def powerCharge(self):
-        return self.__powerCharge
-    @powerCharge.setter
-    def powerCharge(self, powerCharge):
-        if powerCharge < 0:
-            raise ValueError("ES powerCharge must be positive.")
-        self.__powerCharge = powerCharge
-
-    @property
-    def powerDisch(self):
-        return self.__powerDisch
-    @powerDisch.setter
-    def powerDisch(self, powerDisch):
-        if powerDisch < 0:
-            raise ValueError("ES powerDisch must be positive.")
-        self.__powerDisch = powerDisch
-
-    @property
-    def rocPCharge(self):
-        return self.__rocPCharge
-    @rocPCharge.setter
-    def rocPCharge(self, rocPCharge):
-        if rocPCharge < 0:
-            raise ValueError("ES rocPCharge must be positive.")
-        self.__rocPCharge = rocPCharge
-
-    @property
-    def rocPDisch(self):
-        return self.__rocPDisch
-    @rocPDisch.setter
-    def rocPDisch(self, rocPDisch):
-        if rocPDisch < 0:
-            raise ValueError("ES rocPDisch must be positive.")
-        self.__rocPDisch = rocPDisch
-
-
-class LD:
-    '''
-    The LD object defines and stores parameters of electric loads
-
-    Parameters
-    ----------
-    continuous : float
-        Installed power of continuous loads in Watts.
-    flexible : float
-        Installed power of flexible loads in Watts.        
-    largest : float
-        Installed power of the largest load in Watts.        
     powerIn : float
-        Current active power demand in Watts.                    
+        Current power input in Watts.
+        If = 0, switches off ES.
+        If < 0, ES is discharging.
+        If > 0, ES is charging.
+    
     '''
-    def __init__(self, continuous, flexible, largest, powerIn=0.0):
+    def __init__(self, outMax, outMin, powerIn=0):
         self.__initComplete = False
-        self.continuous = continuous
-        self.flexible = flexible
-        self.largest = largest
+        self.outMax = outMax
+        self.outMin = outMin
         self.powerIn = powerIn
 
         self.__initComplete = True
 
     @property
-    def continuous(self):
-        return self.__continuous
-    @continuous.setter
-    def continuous(self, continuous):
-        if continuous < 0:
-            raise ValueError("LD continuous cannot be negative.")
-        self.__continuous = continuous
+    def outMax(self):
+        return self.__outMax
+    @outMax.setter
+    def outMax(self, outMax):
+        if outMax < 0:
+            raise ValueError("ES outMax must be positive.")
+        self.__outMax = outMax
 
     @property
-    def flexible(self):
-        return self.__flexible
-    @flexible.setter
-    def flexible(self, flexible):
-        if flexible < 0:
-            raise ValueError("LD flexible cannot be negative.")
-        self.__flexible = flexible
-
-    @property
-    def largest(self):
-        return self.__largest
-    @largest.setter
-    def largest(self, largest):
-        if largest < 0:
-            raise ValueError("LD largest cannot be negative.")
-        elif largest > max(self.continuous, self.flexible):
-            raise ValueError("LD largest is invalid.")
-        self.__largest = largest
+    def outMin(self):
+        return self.__outMin
+    @outMin.setter
+    def outMin(self, outMin):
+        if outMin < 0:
+            raise ValueError("ES outMin must be positive.")
+        self.__outMin = outMin
 
     @property
     def powerIn(self):
         return self.__powerIn
     @powerIn.setter
     def powerIn(self, powerIn):
-        if powerIn < 0:
-            raise ValueError("LD powerIn cannot be negative.")
-        elif powerIn > (self.continuous + self.flexible):
-            raise ValueError("LD powerIn is invalid.")
-        self.__powerIn = powerIn
+        if (powerIn == 0) or (-self.outMin <= powerIn <= self.outMax):
+            self.__powerIn = powerIn 
+        else:
+            self.__powerIn = max(-self.outMin, min(powerIn, self.outMax))
 
 
 class GT:
@@ -146,7 +73,7 @@ class GT:
         Maximum power ouput in Watts.
     powerOut : float
         Current power ouput in Watts.
-        If powerOut = 0, switches off GT. Otherwise, outMin <= powerOut <=outMax
+        If = 0, switches off GT. Otherwise, outMin <= powerOut <=outMax
 
     Attributes
     ----------
@@ -155,12 +82,13 @@ class GT:
     inertia : float
         Inertia constant of the turbogenerator in seconds.
     rocP : float
-        Maximum rate of change of power in Watts per second.
+        Maximum rate of change of power in pu of powerRated per second.
     
     '''
 
     def __init__(self, model, outMin, outMax, powerOut=0):
         self.__initComplete = False
+        self.__powerOut = 0
         self.model = model
         self.outMin = outMin
         self.outMax = outMax
@@ -202,14 +130,14 @@ class GT:
             #  Document 23380E-KVEST-251-E-DS-00002
             self.__powerRated = 30.8e6
             self.__inertia = 1.85
-            self.__rocP = 0.15 * self.powerRated 
+            self.__rocP = 0.15 
         elif model == 'LM6000':
             # Based on the document "LM6000-60 HZ Gas Turbine Generator Set
             #  Product Specification" and Cigré Report 238 "Modeling of Gas
             #  Turbines and Steam Turbines in Combined-Cycle Power Plants"
             self.__powerRated = 44.7e6
             self.__inertia = 1.8
-            self.__rocP = 0.15 * self.powerRated 
+            self.__rocP = 0.15 
         else:
             raise ValueError("GT model is invalid.")
     
@@ -262,6 +190,71 @@ class GT:
             self.__powerOut = max(self.outMin, min(powerOut, self.outMax))
 
 
+class LD:
+    '''
+    The LD object defines and stores parameters of electric loads
+
+    Parameters
+    ----------
+    continuous : float
+        Installed power of continuous loads in Watts.
+    largest : float
+        Installed power of the largest continuous load in Watts.        
+    flexible : float
+        Installed power of flexible loads in Watts.        
+    powerIn : float
+        Current active power demand in Watts.                    
+    '''
+    def __init__(self, continuous, largest, flexible, powerIn=0.0):
+        self.__initComplete = False
+        self.continuous = continuous
+        self.largest = largest
+        self.flexible = flexible
+        self.powerIn = powerIn
+
+        self.__initComplete = True
+
+    @property
+    def continuous(self):
+        return self.__continuous
+    @continuous.setter
+    def continuous(self, continuous):
+        if continuous < 0:
+            raise ValueError("LD continuous cannot be negative.")
+        self.__continuous = continuous
+
+    @property
+    def largest(self):
+        return self.__largest
+    @largest.setter
+    def largest(self, largest):
+        if largest < 0:
+            raise ValueError("LD largest cannot be negative.")
+        elif largest > self.continuous:
+            raise ValueError("LD largest is invalid.")
+        self.__largest = largest
+
+    @property
+    def flexible(self):
+        return self.__flexible
+    @flexible.setter
+    def flexible(self, flexible):
+        if flexible < 0:
+            raise ValueError("LD flexible cannot be negative.")
+        self.__flexible = flexible
+
+    @property
+    def powerIn(self):
+        return self.__powerIn
+    @powerIn.setter
+    def powerIn(self, powerIn):
+        if powerIn < 0:
+            raise ValueError("LD powerIn cannot be negative.")
+        elif powerIn > (self.continuous + self.flexible):
+            raise ValueError("LD powerIn is invalid.")
+        self.__powerIn = powerIn
+
+
 class WT:
     '''
     The WT object defines and stores parameters of a wind turbine
@@ -272,6 +265,8 @@ class WT:
         Wind turbine model.
         'NREL': NREL 5MW
         'Hywind': Hywind Tampen 6MW
+    windSpeed : float
+        Current wind speed at hub height in meters per second. 
 
     Attributes
     ----------
@@ -279,29 +274,43 @@ class WT:
         Rated power of the wind turbine in Watts. 
     
     '''
-    def __init__(self, model):
+    def __init__(self, model, windSpeed = 0):
         self.__initComplete = False
         self.model = model
+        self.windSpeed = windSpeed
 
         self.__initComplete = True
 
-    def powerOut(self, windSpeed):
+    def powerOut(self, *args):
         '''
         Given the wind speed at hub height, provides the output power.
+        If only one input is given, stores it for future use.
+        If no argument is given, outputs the power of the stored wind speed.
+        If several arguments are given, return a list of output powers.   
 
         Parameters
         ----------
-        windSpeed : float
+        *args : list of float
             The wind speed at hub height in meters per second.
 
         Returns
         ----------
-        powerOut : float
+        powerOut : list of float
             The ouput power in Watts.
 
         '''
-        
-        return np.interp(windSpeed, self.__inWindSpeed, self.__outPower, left=0, right=0) * self.powerRated
+
+        powerOut = []
+        for _ in args:
+            powerOut.append(np.interp( _, self.__pcurveWindSpeed, self.__pcurvePower, left=0, right=0) * self.powerRated)         
+
+        if len(powerOut) == 0:
+            powerOut = np.interp(self.windSpeed, self.__pcurveWindSpeed, self.__pcurvePower, left=0, right=0) * self.powerRated
+        elif len(powerOut) == 1:
+            powerOut = powerOut[0]
+            self.__windSpeed = args[0]
+
+        return powerOut
 
     @property
     def model(self):
@@ -312,15 +321,25 @@ class WT:
         if model == 'NREL':
             # Based on report NREL/TP-500-38060
             self.__powerRated = 5e6
-            self.__inWindSpeed = list(range(2,12)) + [25,26]
-            self.__outPower = [0, 0.034, 0.0782, 0.1462, 0.2346, 0.3504, 0.5068, 0.6904, 0.9116, 1, 1, 0]
+            self.__pcurveWindSpeed = list(range(2,12)) + [25,26]
+            self.__pcurvePower = [0, 0.034, 0.0782, 0.1462, 0.2346, 0.3504, 0.5068, 0.6904, 0.9116, 1, 1, 0]
         elif model == 'Hywind':
             # Based on based on https://www.uib.no/sites/w3.uib.no/files/attachments/hywind_energy_lab.pdf (page 25)
             self.__powerRated = 6e6
-            self.__inWindSpeed = list(range(3,16)) + [25,26]
-            self.__outPower = [0, 0.029, 0.0725, 0.1304, 0.2101, 0.3261, 0.4638, 0.6232, 0.7754, 0.8913, 0.9565, 0.9855, 1, 1, 0]
+            self.__pcurveWindSpeed = list(range(3,16)) + [25,26]
+            self.__pcurvePower = [0, 0.029, 0.0725, 0.1304, 0.2101, 0.3261, 0.4638, 0.6232, 0.7754, 0.8913, 0.9565, 0.9855, 1, 1, 0]
         else:
             raise ValueError("WT model is invalid.")
+
+    @property
+    def windSpeed(self):
+        return self.__windSpeed
+    @windSpeed.setter
+    def windSpeed(self, windSpeed):
+        if 0 <= windSpeed <= 50:
+            self.__windSpeed = windSpeed 
+        else:
+            self.__windSpeed = 0
 
     @property
     def powerRated(self):
@@ -336,9 +355,9 @@ def elgridEval(freqMax, freqMin, GT, WT, ES, LD):
     Parameters
     ----------
     freqMax: float
-        Maximum frequency allowed in Hertz.
+        Maximum frequency allowed in per unit.
     freqMin: float
-        Minimum frequency allowed in Hertz.        
+        Minimum frequency allowed in per unit.        
     GT : GT
         Array of gas turbine objects.
     WT : WT
@@ -354,31 +373,51 @@ def elgridEval(freqMax, freqMin, GT, WT, ES, LD):
 
     '''
     
-    ## Initialize local variables    
-    # Base power in MVA
+    # Base power in MVA for per unit (pu) / normalization
     SBASE = 100e6
 
-    # GTs aggregated power in per unit of SBASE
+    # Calculation of GTs aggregated values     
     powerGTpu = 0
-    # GTs aggregated inertia in per unit of SBASE
     inertiaGTpu = 0
-    # GTs minimum rocP
-    rocPGT = 1e20
-
-    # WTs aggregated power in per unit of SBASE
-    powerGTpu = 0
-
-    ## Calculation of aggregated values     
+    rocPGTpu = 1e20
     for _ in GT:
-        powerGTpu += _.powerRated / SBASE
-        inertiaGTpu += ( _.inertia * _.powerRated / SBASE)
-        rocPGT = min(rocPGT, _.rocP)
+        powerGTpu += _.powerOut
+        inertiaGTpu += _.inertia
+        rocPGTpu = min(rocPGTpu, _.rocP * _.powerRated)
+    powerGTpu /= SBASE
+    inertiaGTpu *= _.powerRated / SBASE
+    rocPGTpu /= SBASE
         
+    # Calculation of WTs aggregated values     
+    powerWTpu = 0
+    maxpowerOutStepWT = 0
     for _ in WT:
-        powerWTpu += _.powerRated / SBASE
-         
+        powerWTpu += _.powerOut()
+        maxpowerOutStepWT = max(maxpowerOutStepWT, _.powerOut())
+    powerWTpu /= SBASE
+    maxpowerOutStepWT /= SBASE 
+             
+    # Calculation of ESs aggregated values     
+    powerESpu = 0
+    maxpowerOutStepES = 0
+    maxpowerInStepES = 0
+    for _ in ES:
+        powerESpu += _.powerIn
+        if _.powerIn < 0:
+            maxpowerOutStepES = max(maxpowerOutStepES, -_.powerIn)
+        else:
+            maxpowerInStepES = max(maxpowerInStepES, _.powerIn)
+    powerESpu /= SBASE
+    maxpowerOutStepES /= SBASE 
+    maxpowerInStepES /= SBASE
+    
+    # Calculation of ESs aggregated values     
+    powerLDpu = 0
+    for _ in LD:
+        powerLDpu += _.powerIn
+    powerLDpu /= SBASE
 
-    return True
+    # Calculate the worst possible load increase   
         
         
         
--- test_elgrid.py
+++ test_elgrid.py
@@ -1,6 +1,7 @@
 import elgrid_eval as elgev
 
 # Test area for GT class
+print('## GT class ##')
 testGT = elgev.GT('LM2500', 10e6, 30e6)
 print(testGT.inertia)
 print(testGT.rocP)
@@ -10,23 +11,41 @@ testGT.outMin = 20e6
 print(testGT.outMin)
 
 # Test area for WT class
+print('## WT class ##')
 testWT = elgev.WT('Hywind')
+print(testWT.windSpeed)
+print(testWT.powerOut())
+print(testWT.powerOut([3,4]))
 print(testWT.powerOut(12))
+print(testWT.windSpeed)
+print(testWT.powerOut())
+
 
 # Test area for ES class
-testES = elgev.ES(6e6, 2.5e6, 6e6/300, 2.5e6/300)
-print(testES.powerCharge)
-print(testES.rocPDisch)
+print('## ES class ##')
+testES = elgev.ES(6e6, 2.5e6, 1.3478e6)
+testES.powerOut = -3e6 
+print(testES.powerIn)
 
 # Test area for LD class
-testLD = elgev.LD(36.8e6, 6.8e6, 7e6)
+print('## LD class ##')
+testLD = elgev.LD(36.8e6, 7e6, 6.8e6)
 print(testLD.continuous)
 testLD.powerIn = 10e6
 print(testLD.powerIn)
 
-# Test area for elgridEval method
-myTestGTs = [('LM2500', 10e6, 30e6), ('LM2500', 10e6, 30e6)]
-myGTList = [elgev.GT(_model,_outMin,_outMax) for (_model,_outMin,_outMax) in myTestGTs]
+# Test area for elgridEval function
+print('## elgridEval function ##')
+myTestGTs = [('LM2500', 10e6, 30e6, 15e6), ('LM6000', 10e6, 30e6, 15e6)]
+myGTList = [elgev.GT(_model, _outMin, _outMax, _powerOut) for (_model, _outMin, _outMax, _powerOut) in myTestGTs]
 
-myTestLDs = [(36.8e6, 6.8e6, 7e6)]
-myLDList = [elgev.LD(_continuous,_flexible,_largest) for (_continuous,_flexible,_largest) in myTestLDs]
+myTestWTs = [('Hywind', 12), ('NREL', 12)]
+myWTList = [elgev.WT(_model, _windSpeed) for (_model, _windSpeed) in myTestWTs]
+
+myTestESs = [(6e6, 0, 1.3478e6), (0, 2.5e6, 0)]
+myESList = [elgev.ES(_outMax, _outMin, _powerIn) for (_outMax, _outMin, _powerIn) in myTestESs]
+
+myTestLDs = [(36.8e6, 6.8e6, 7e6, 39e6)]
+myLDList = [elgev.LD(_continuous, _largest, _flexible, _powerIn) for (_continuous, _largest, _flexible, _powerIn) in myTestLDs]
+
+elgev.elgridEval(0.02, -0.02, myGTList, myWTList, myESList, myLDList)
