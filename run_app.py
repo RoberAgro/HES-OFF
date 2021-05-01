c37e0734b286d1a2258cd661616cee53dd52ab49
Roberto Agromayor <roberto.agromayor@ntnu.no>
2021-05-01 13:50:09 +0200

    update run file

--- run_app.py
+++ run_app.py
@@ -1,4 +1,4 @@
 # Run the app locally at http://127.0.0.1:8080/
+from hes_off_app import app
 if __name__ == "__main__":
-    from hes_off_app import app
     app.run(host="127.0.0.1", port=8080, debug=True)
