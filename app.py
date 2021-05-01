481e6b560b76ca97657edbfa540e1df524e2565f
Roberto Agromayor <roberto.agromayor@ntnu.no>
2021-05-01 14:43:10 +0200

    Update files for Heroku deployment

---
+++ app.py
@@ -0,0 +1,4 @@
+from hes_off_app import app
+if __name__ == "__main__":
+    # Run the app locally at http://127.0.0.1:5000/
+    app.run(host="127.0.0.1", port=5000, debug=True)
--- run_app.py
+++
@@ -1,4 +0,0 @@
-# Run the app locally at http://127.0.0.1:8080/
-from hes_off_app import app
-if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080, debug=True)
